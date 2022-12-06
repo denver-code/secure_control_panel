dd9481514d57f7f66777d1f532923a2593cbae72
denver-code <csigorek@gmail.com>
2022-12-06 22:23:11 +0000

    Added user info alert widget

---
+++ lib/internal/components/widgets/user_info_alert_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class UserInfoAlertWidget extends StatelessWidget {
+  const UserInfoAlertWidget({Key? key, required this.user}) : super(key: key);
+  final user;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text("User information"),
+      content: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // TODO: Write generator for that stuff
+          Text("Username: ${user["username"]}"),
+          Text("Department: ${user["department"]}"),
+          Text("Access: ${user["access"]}"),
+          Text("Family: ${user["family"]}"),
+          Text("Type: ${user["type"]}"),
+          const Text("Permissions: coming soon..")
+        ],
+      ),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () => Navigator.pop(context, "OK"),
+          child: const Text(
+            "OK",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ],
+    );
+  }
+}
