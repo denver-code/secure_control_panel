d9516bb7a99d40543a92a03feb286ae28d4ec634
denver-code <csigorek@gmail.com>
2022-12-09 19:54:01 +0000

    authorisationScreen -> authorisation

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:secure_control_panel/internal/applications/applications_overview_application.dart'
+    as applications_app;
 import 'package:secure_control_panel/screens/authorisation_screen.dart';
-import 'package:secure_control_panel/screens/debug_screen.dart';
 import 'package:secure_control_panel/screens/desktop_screen.dart';
+
+import 'internal/applications/debug_application.dart' as debug_app;
+import 'internal/applications/test_applications/all_widget_application.dart';
+import 'internal/applications/test_applications/route_load_application.dart'
+    as route_load;
 
 class Config {
   static const kDebugMode = true;
@@ -25,12 +31,34 @@ class Config {
       "appRoute": "/debug",
       "appIconText": "D",
       "isNotHidden": kDebugMode,
+      "isNotTest": true,
+      "details": debug_app.AppManifest.details
+    },
+    {
+      "appName": "Route Loader",
+      "appRoute": "/routeLoad",
+      "appIconText": "R",
+      "isNotHidden": true,
+      "isNotTest": false,
+      "details": route_load.AppManifest.details
+    },
+    {
+      "appName": "Applications",
+      "appRoute": "/applicationsOverview",
+      "appIconText": "A",
+      "isNotHidden": true,
+      "isNotTest": true,
+      "details": applications_app.AppManifest.details
     },
   ];
 
   static Map<String, Widget Function(BuildContext)> routes = {
-    "/authorisationScreen": (_) => const AuthorisationScreen(),
+    "/authorisation": (_) => const AuthorisationScreen(),
     "/desktop": (_) => const DesktopScreen(),
-    "/debug": (_) => const DebugScreen()
+    "/debug": (_) => const debug_app.DebugApplication(),
+    "/allWidgetsApp": (_) => const AllWidgetTestApplicaton(),
+    "/routeLoad": (_) => route_load.RouteLoaderApplication(),
+    "/applicationsOverview": (_) =>
+        const applications_app.ApplicationsOverviewApplication()
   };
 }
