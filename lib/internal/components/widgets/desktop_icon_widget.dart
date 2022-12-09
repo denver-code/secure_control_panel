2adec162b98c954977724cf7d3b99e97ff3cb97c
denver-code <csigorek@gmail.com>
2022-12-09 00:32:25 +0000

    Template -> work dekstop feature, Desktop_Screen

--- lib/internal/components/widgets/desktop_icon_widget.dart
+++ lib/internal/components/widgets/desktop_icon_widget.dart
@@ -3,34 +3,54 @@ import 'package:flutter/material.dart';
 import 'delimiter_widget.dart';
 
 class DesktopIconWidget extends StatelessWidget {
-  const DesktopIconWidget({Key? key}) : super(key: key);
-
+  const DesktopIconWidget(
+      {Key? key,
+      required this.appName,
+      required this.appRoute,
+      required this.appIconText})
+      : super(key: key);
+  final String appName;
+  final String appRoute;
+  final String appIconText;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            width: 50,
-            height: 50,
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.white,
-                width: 2,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed(appRoute);
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.white,
+                  width: 2,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 2.2),
+                child: Text(
+                  appIconText,
+                  style: const TextStyle(color: Colors.white, fontSize: 35),
+                ),
               ),
             ),
-          ),
-          const DelimiterWidget(height: 5),
-          const Text(
-            "Example App",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 13,
+            const DelimiterWidget(height: 7),
+            Text(
+              appName,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 13,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/screens/desktop_screen.dart
+++ lib/screens/desktop_screen.dart
@@ -1,6 +1,5 @@
 import "package:flutter/material.dart";
 import "package:secure_control_panel/config.dart";
-import 'package:secure_control_panel/internal/components/widgets/delimiter_widget.dart';
 import 'package:secure_control_panel/internal/components/widgets/desktop_icon_widget.dart';
 import '../internal/components/layouts/main_layout.dart';
 
@@ -8,6 +7,7 @@ class DesktopScreen extends StatelessWidget {
   const DesktopScreen({Key? key}) : super(key: key);
 
   final _user = Config.guestOfflineUser;
+  final _apps = Config.applicationList;
 
   @override
   Widget build(BuildContext context) {
@@ -18,94 +18,31 @@ class DesktopScreen extends StatelessWidget {
         height: MediaQuery.of(context).size.height / 1.3,
         exitButtonCallback: () {},
         contentWidgetList: [
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                    DesktopIconWidget(),
-                  ],
-                ),
-              ],
+          IntrinsicHeight(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width / 1.6,
+              height: MediaQuery.of(context).size.height / 1.3,
+              // color: Colors.white30,
+              child: GridView.count(
+                crossAxisCount: 7,
+                childAspectRatio: 1.0,
+                padding: const EdgeInsets.all(4.0),
+                mainAxisSpacing: 4.0,
+                crossAxisSpacing: 4.0,
+                children: (() {
+                  return List.generate(_apps.length, (index) {
+                    if (_apps[index]["isNotHidden"]) {
+                      return DesktopIconWidget(
+                        appName: _apps[index]["appName"] ?? "",
+                        appRoute: _apps[index]["appRoute"] ?? "",
+                        appIconText: _apps[index]["appIconText"] ?? "",
+                      );
+                    } else {
+                      return Container();
+                    }
+                  });
+                }()),
+              ),
             ),
           ),
         ]);
