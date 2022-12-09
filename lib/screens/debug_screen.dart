9bfd19c1be2c38e27fc589c73ac22082b7795718
denver-code <csigorek@gmail.com>
2022-12-09 00:32:45 +0000

    Added routeList and AppInfo on debugScreen

--- lib/screens/debug_screen.dart
+++ lib/screens/debug_screen.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:secure_control_panel/config.dart';
 import 'package:secure_control_panel/internal/components/layouts/main_layout.dart';
 
-class DebugScreen extends StatelessWidget {
+class DebugScreen extends StatefulWidget {
   const DebugScreen({Key? key}) : super(key: key);
 
   @override
+  State<DebugScreen> createState() => _DebugScreenState();
+}
+
+class _DebugScreenState extends State<DebugScreen> {
+  final List _routes = Config.routes.keys.toList();
+  late PackageInfo packageInfo;
+
+  @override
+  void initState() {
+    super.initState();
+    _getAppInfo();
+  }
+
+  _getAppInfo() async {
+    PackageInfo pi = await PackageInfo.fromPlatform();
+    setState(() {
+      packageInfo = pi;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    List appInfo = [
+      "AppName: ${packageInfo.appName}",
+      "PackageName: ${packageInfo.packageName}",
+      "Version: ${packageInfo.version}",
+      "BuildNumber: ${packageInfo.buildNumber}",
+    ];
+
     return MainLayout(
         windowName: "Debug screen",
         width: MediaQuery.of(context).size.width / 1.6,
@@ -14,7 +43,60 @@ class DebugScreen extends StatelessWidget {
         exitButtonCallback: () {
           Navigator.of(context).pop();
         },
-        contentWidgetList: const [],
+        contentWidgetList: [
+          const Center(
+            child: Text(
+              "Application Info",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 17,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width / 1.6,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: (() {
+                return List.generate(appInfo.length, (index) {
+                  return Text(
+                    appInfo[index],
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  );
+                });
+              }()),
+            ),
+          ),
+          const Center(
+            child: Text(
+              "Avaible Routes",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 17,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width / 1.6,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: (() {
+                return List.generate(_routes.length, (index) {
+                  return Text(
+                    "Route #$index - ${_routes[index]}",
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  );
+                });
+              }()),
+            ),
+          )
+        ],
         user: Config.guestOfflineUser);
   }
 }
