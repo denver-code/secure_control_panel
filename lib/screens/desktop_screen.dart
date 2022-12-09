04c17dca608819d71333f399b9786becc426fa3a
denver-code <csigorek@gmail.com>
2022-12-09 19:53:05 +0000

    Added exit btn callback and new desktop generator

--- lib/screens/desktop_screen.dart
+++ lib/screens/desktop_screen.dart
@@ -1,6 +1,6 @@
 import "package:flutter/material.dart";
 import "package:secure_control_panel/config.dart";
-import 'package:secure_control_panel/internal/components/widgets/desktop_icon_widget.dart';
+import 'package:secure_control_panel/internal/components/widgets/icon_grid_widget.dart';
 import '../internal/components/layouts/main_layout.dart';
 
 class DesktopScreen extends StatelessWidget {
@@ -16,35 +16,10 @@ class DesktopScreen extends StatelessWidget {
         windowName: "Desktop",
         width: MediaQuery.of(context).size.width / 1.6,
         height: MediaQuery.of(context).size.height / 1.3,
-        exitButtonCallback: () {},
-        contentWidgetList: [
-          IntrinsicHeight(
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width / 1.6,
-              height: MediaQuery.of(context).size.height / 1.3,
-              // color: Colors.white30,
-              child: GridView.count(
-                crossAxisCount: 7,
-                childAspectRatio: 1.0,
-                padding: const EdgeInsets.all(4.0),
-                mainAxisSpacing: 4.0,
-                crossAxisSpacing: 4.0,
-                children: (() {
-                  return List.generate(_apps.length, (index) {
-                    if (_apps[index]["isNotHidden"]) {
-                      return DesktopIconWidget(
-                        appName: _apps[index]["appName"] ?? "",
-                        appRoute: _apps[index]["appRoute"] ?? "",
-                        appIconText: _apps[index]["appIconText"] ?? "",
-                      );
-                    } else {
-                      return Container();
-                    }
-                  });
-                }()),
-              ),
-            ),
-          ),
-        ]);
+        exitButtonCallback: () {
+          Navigator.of(context)
+              .pushNamedAndRemoveUntil("/authorisation", (route) => false);
+        },
+        contentWidgetList: [IconGridWidget(apps: _apps)]);
   }
 }
